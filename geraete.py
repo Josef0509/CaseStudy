43b810a470d180a518cdb69c93563517d19eab9f
STEVES_LENOVO\steve <steveposchstev@outlook.at>
2024-01-12 11:36:55 +0100

    First Device Logic

--- geraete.py
+++ geraete.py
@@ -2,7 +2,8 @@ import streamlit as st
 from datetime import datetime
 import time
 from st_pages import add_page_title
-import sys
+from devices import Device
+import queries
 
 # Initialize session state
 if 'show_session' not in st.session_state:
@@ -23,16 +24,7 @@ button1_ph = cl1.empty()
 button2_ph = cl2.empty()
 button3_ph = cl3.empty()
 
-#Fake data
-device_data = {
-	"name": ["Gerät 1", "Gerät 2", "Gerät 3", "Gerät 4"],
-	"responsible_person": ["Person 1", "Person 2", "Person 3", "Person 2"],
-	"__creation_date": [datetime(2021, 1, 1), datetime(2022, 3, 13), datetime(2020, 6, 30), datetime(2023, 10, 4)],
-	"article_number": ["123456", "234567", "345678", "456789"],
-	"acquisition_date": [datetime(2021, 1, 1), datetime(2022, 3, 13), datetime(2020, 6, 30), datetime(2023, 10, 4)],
-	"description": ["Dies ist ein Testgerät", "Dies ist ein Testgerät", "Dies ist ein Testgerät", "Dies ist ein Testgerät"]
-	# Weitere Attribute hier hinzufügen
-}
+dateformat = "%Y-%m-%d "
 
 person_data = {
 	"name": ["Person 1", "Person 2", "Person 3"],
@@ -42,20 +34,14 @@ if st.session_state.show_session == 0:
 	header_ph.header("Neues Gerät anlegen")
 	device_name = device_ph.text_input("Name des Geräts:")
 	article_number = article_number_ph.text_input("Artikelnummer:")
-	acquisition_date = acquisition_date_ph.date_input("Anschaffungsdatum:")
+	acquisition_date = datetime.now().strftime(dateformat)
+	change_date = datetime.now().strftime(dateformat)
 	device_description = description_ph.text_area("Optionale Beschreibung:")
 	device_responsible = responsible_person_ph.selectbox("verantwortliche Person:", person_data["name"])
 
 	if button1_ph.button("Gerät anlegen/ändern"):
-		device_data = {
-			"name": device_name,
-			"responsible_person": device_responsible,
-			"__creation_date": datetime.now(),
-			"article_number": article_number,
-			"acquisition_date": acquisition_date,
-			"description": device_description
-			# Weitere Attribute hier hinzufügen
-		}
+		data = Device(device_name, article_number, device_description, device_responsible, acquisition_date, change_date)
+		data.store_data()
 
 		with st.spinner("Loading..."):
 			time.sleep(1)
@@ -93,33 +79,28 @@ if st.session_state.show_session == 0:
 
 if st.session_state.show_session == 1:
 	header_ph.header("Gerät ändern")
-	device_name = device_ph.selectbox("Gerät:", device_data["name"], placeholder="Gerät auswählen ...")
+	devices = queries.find_devices()
+	device_name = device_ph.selectbox("Gerät:", devices, placeholder="Gerät auswählen ...")
 
-	if device_name in device_data["name"]:
-		device_responsible = device_data["responsible_person"][device_data["name"].index(device_name)]
-		device_article_number = device_data["article_number"][device_data["name"].index(device_name)]
-		device_acquisition_date = device_data["acquisition_date"][device_data["name"].index(device_name)]
-		device_description = device_data["description"][device_data["name"].index(device_name)]
+	device_data = Device.load_data_by_device_name(device_name)
+	device_data.article_number = article_number_ph.text_input("Artikelnummer:", value = device_data.article_number)
+	acquisition_date_ph.text(device_data.acquisition_date)
+	device_data.change_date = datetime.now().strftime(dateformat)
+	device_data.device_description = description_ph.text_area("Optionale Beschreibung:", value = device_data.device_description)
+	device_data.managed_by_user_id = responsible_person_ph.selectbox("verantwortliche Person:", person_data["name"], index = person_data["name"].index(device_data.managed_by_user_id))
+	#
+	
+	if button1_ph.button("Gerät ändern"):
+		device_data.store_data()
 
-		device_article_number = article_number_ph.text_input("Artikelnummer:", value = device_article_number)
-		device_acquisition_date = acquisition_date_ph.date_input("Anschaffungsdatum:", value= device_acquisition_date)
-		device_description = description_ph.text_area("Optionale Beschreibung:", value = device_description)
-		device_responsible = responsible_person_ph.selectbox("verantwortliche Person:", device_data["responsible_person"], index = device_data["responsible_person"].index(device_responsible))
-
-		if button1_ph.button("Gerät ändern"):
-			device_data["article_number"][device_data["name"].index(device_name)] = device_article_number
-			device_data["acquisition_date"][device_data["name"].index(device_name)] = device_acquisition_date
-			device_data["description"][device_data["name"].index(device_name)] = device_description
-			device_data["responsible_person"][device_data["name"].index(device_name)] = device_responsible
-
-			with st.spinner("Loading..."):
-				time.sleep(1)
-				#Save the device
-
-			st.success(f"Gerät {device_name} mit dem Verantwortlichen {device_responsible} wurde aktualisiert!")
-			time.sleep(2)
-			st.session_state.show_session = 0
-			st.rerun()
+		with st.spinner("Loading..."):
+			time.sleep(1)
+			#Save the device
+   
+		st.success(f"Gerät {device_data.device_name} mit dem Verantwortlichen {device_data.managed_by_user_id} wurde aktualisiert!")
+		time.sleep(2)
+		st.session_state.show_session = 0
+		st.rerun()
 
 	if button2_ph.button("Zurück"):
 		st.session_state.show_session = 0
@@ -127,16 +108,18 @@ if st.session_state.show_session == 1:
 
 if st.session_state.show_session == 2:
 	header_ph.header("Geräte anzeigen")
-	list_of_tabs = st.tabs(device_data["name"])
+	devices = queries.find_devices()
+	list_of_tabs = st.tabs(devices)
 
 	for i in range(len(list_of_tabs)):
 		with list_of_tabs[i]:
+			device_data = Device.load_data_by_device_name(devices[i])
 
-			st.title(device_data["name"][i])
-			st.text("ID: " + str(i))
-			st.text("Verantwortliche Person: " + device_data["responsible_person"][i])
-			st.text("letzte Änderung: " + str(device_data["__creation_date"][i]))
-			st.text("Anschaffungsdatum: " + str(device_data["acquisition_date"][i]))
+			st.title(device_data.device_name)
+			st.text("ID: " + device_data.article_number)
+			st.text("Verantwortliche Person: " + device_data.managed_by_user_id)
+			st.text("letzte Änderung: " + device_data.change_date)
+			st.text("Anschaffungsdatum: " + device_data.acquisition_date)
 
 	if button1_ph.button("Zurück"):
 		st.session_state.show_session = 0
