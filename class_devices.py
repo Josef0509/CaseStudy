11dd6f6e3196236e5ba41bdf83cb76cffdc8f9c3
stefaPo-cl <steveposchstev@outlook.at>
2024-01-15 05:34:43 +0100

    third page complete

--- class_devices.py
+++ class_devices.py
@@ -6,54 +6,62 @@ from serializer import serializer
 
 
 class Device():
-    # Class variable that is shared between all instances of the class
-    db_connector = TinyDB(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'database.json'), storage=serializer).table('devices')
+	# Class variable that is shared between all instances of the class
+	db_connector = TinyDB(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'database.json'), storage=serializer).table('devices')
 
-    # Constructor
-    def __init__(self, device_name : str, article_number_dev : str, device_description_dev : str, managed_by_user_id : str, acquisition_date_dev: datetime, change_date_dev: datetime):
-        self.device_name = device_name
-        self.article_number = article_number_dev
-        self.acquisition_date = acquisition_date_dev
-        self.change_date = change_date_dev
-        self.device_description = device_description_dev
-        self.managed_by_user_id = managed_by_user_id
-        self.is_active = True
-        
-    # String representation of the class
-    def __str__(self):
-        return f'Device {self.device_name} ({self.managed_by_user_id})'
+	# Constructor
+	def __init__(self, device_name : str, article_number_dev : str, device_description_dev : str, managed_by_user_id : str, acquisition_date_dev: datetime, change_date_dev: datetime):
+		self.device_name = device_name
+		self.article_number = article_number_dev
+		self.acquisition_date = acquisition_date_dev
+		self.change_date = change_date_dev
+		self.device_description = device_description_dev
+		self.managed_by_user_id = managed_by_user_id
+		self.is_active = True
 
-    # String representation of the class
-    def __repr__(self):
-        return self.__str__()
-    
-    def store_data(self):
-        print("Storing data...")
-        # Check if the device already exists in the database
-        DeviceQuery = Query()
-        result = self.db_connector.search(DeviceQuery.device_name == self.device_name)
-        if result:
-            # Update the existing record with the current instance's data
-            result = self.db_connector.update(self.__dict__, doc_ids=[result[0].doc_id])
-            print("Data updated.")
-        else:
-            # If the device doesn't exist, insert a new record
-            self.db_connector.insert(self.__dict__)
-            print("Data inserted.")
-            
-    # Class method that can be called without an instance of the class to construct an instance of the class
-    @classmethod
-    def load_data_by_device_name(cls, device_name):
-        # Load data from the database and create an instance of the Device class
-        DeviceQuery = Query()
-        result = cls.db_connector.search(DeviceQuery.device_name == device_name)
+	# String representation of the class
+	def __str__(self):
+		return f'Device {self.device_name} ({self.managed_by_user_id})'
 
-        if result:
-            data = result[0]
-            return cls(data['device_name'], data['article_number'], data['device_description'], data['managed_by_user_id'], data['acquisition_date'], data['change_date'])
-        else:
-            return None
+	# String representation of the class
+	def __repr__(self):
+		return self.__str__()
 
+	def store_data(self):
+		print("Storing data...")
+		# Check if the device already exists in the database
+		DeviceQuery = Query()
+		result = self.db_connector.search(DeviceQuery.device_name == self.device_name)
+		if result:
+			# Update the existing record with the current instance's data
+			result = self.db_connector.update(self.__dict__, doc_ids=[result[0].doc_id])
+			print("Data updated.")
+		else:
+			# If the device doesn't exist, insert a new record
+			self.db_connector.insert(self.__dict__)
+			print("Data inserted.")
 
+	def delete_data(self):
+		print("Deleting data...")
+		# Check if the device already exists in the database
+		UserQuery = Query()
+		result = self.db_connector.search(UserQuery.name == self.name)
+		if result:
+			# Delete the existing record
+			result = self.db_connector.remove(doc_ids=[result[0].doc_id])
+			print("Data deleted.")
+		else:
+			print("Data not found.")
 
-    
+	# Class method that can be called without an instance of the class to construct an instance of the class
+	@classmethod
+	def load_data_by_device_name(cls, device_name):
+		# Load data from the database and create an instance of the Device class
+		DeviceQuery = Query()
+		result = cls.db_connector.search(DeviceQuery.device_name == device_name)
+
+		if result:
+			data = result[0]
+			return cls(data['device_name'], data['article_number'], data['device_description'], data['managed_by_user_id'], data['acquisition_date'], data['change_date'])
+		else:
+			return None
--- nutzer.py
+++ nutzer.py
@@ -3,6 +3,7 @@ import datetime
 import time
 from st_pages import add_page_title
 from class_user import User
+import queries
 
 # Initialize session state
 if 'show_session' not in st.session_state:
@@ -20,38 +21,47 @@ button2_ph = cl2.empty()
 table_ph = st.empty()
 
 #creating a dropdown menu with all devices
-user_name = name_ph.text_input("Name des Nutzers:")
-user_email = email_ph.text_input("E-Mail-Adresse des Nutzers:")
+user_name = name_ph.text_input("Name des Nutzers:", placeholder="Max Mustermann")
+user_email = email_ph.text_input("E-Mail-Adresse des Nutzers:", placeholder="E-Mail-Adresse")
 
 
 # Nutzer speichern
 if button1_ph.button("Nutzer anlegen"):
-	#Store the user data in a dictionary
-	User(user_name, user_email)
-	User.store_data()
+	if user_name == "" or user_email == "":
+		st.warning("Bitte alle Felder ausfüllen!")
+		time.sleep(2)
+		st.rerun()
+	else:
+		#Store the user data in a dictionary
+		data_user = User(user_name, user_email)
+		data_user.store_data()
 
-	with st.spinner("Loading..."):
-		time.sleep(1)
-		#Save the user
-	st.success(f"Nutzer {user_name} ({user_email}) wurde angelegt!")
-	time.sleep(2)
-	st.rerun()
+		with st.spinner("Loading..."):
+			time.sleep(1)
+			#Save the user
+		st.success(f"Nutzer {user_name} ({user_email}) wurde angelegt!")
+		time.sleep(2)
+		st.rerun()
 
 
 # Nutzerdaten anzeigen
 if button2_ph.button("Nutzer anzeigen"):
 	# Empty placeholder
 	button2_ph.empty()
-	# Load data
-	User.load_data_by_user_name(user_name)
-	# Show data
-	table_ph.data_editor( data_df, column_config=
-	{
-		"Nutzer": st.column_config.ListColumn( "Nutzername", width="medium",),
-		"Email": st.column_config.ListColumn( "E-Mail", width="medium",),
-	},
-	hide_index=True,
-	)
-	# Hide data
-	if button2_ph.button("Nutzer ausblenden"):
-		table_ph.empty()
+	users = queries.find_database('users', 'name')
+
+	if users:
+		data_list = []
+		for user in users:
+			user_data = User.load_data_by_user_name(user)
+			if user_data:
+				data_list.append({"Nutzer": user_data.name, "Email": user_data.email})
+		# Show data
+		table_ph.data_editor( data_list, column_config= {"Nutzer": st.column_config.ListColumn( "Nutzername", width="medium",), "Email": st.column_config.ListColumn( "E-Mail", width="medium",),} ,hide_index=True)
+		# Hide data
+		if button2_ph.button("Nutzer ausblenden"):
+			table_ph.empty()
+	else:
+		st.error("Keine gültigen Nutzer gefunden!")
+		time.sleep(2)
+		st.rerun()
